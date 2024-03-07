a5f2dfae5ea127ed51cd068896c0fdfa933e8e99
IkrlegidO <73440160+Ikrlegido12@users.noreply.github.com>
2024-03-07 19:38:07 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    numero += -1
+})
+input.onButtonPressed(Button.AB, function () {
+    numero = 0
+})
+input.onButtonPressed(Button.B, function () {
+    numero += 1
+})
+let numero = 0
+numero = 0
 basic.forever(function () {
-	
+    basic.showNumber(numero)
+    basic.pause(100)
 })
