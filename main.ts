79b9bb096238dab8e78c4c036f1b40ae9a2988a6
IkrlegidO <73440160+Ikrlegido12@users.noreply.github.com>
2024-03-07 19:43:56 +0100

    Añadido en Agitado y Tocar logo

--- main.ts
+++ main.ts
@@ -1,11 +1,17 @@
 input.onButtonPressed(Button.A, function () {
     numero += -1
 })
+input.onGesture(Gesture.Shake, function () {
+    numero += 10
+})
 input.onButtonPressed(Button.AB, function () {
     numero = 0
 })
 input.onButtonPressed(Button.B, function () {
     numero += 1
+})
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    numero += -10
 })
 let numero = 0
 numero = 0
